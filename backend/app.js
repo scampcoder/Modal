de348c12256cfb7705a426cbf85c19a193f5d987
Victoria Kee <victoriakee95@gmail.com>
2021-04-26 11:23:59 -0500

    Fix typo on add/rmv

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,9 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
 
 modalBtn.addEventListener('click', function(){
-    modalOverlay.classList.add('.open-modal');
+    modalOverlay.classList.add('open-modal');
 })
 
 closeBtn.addEventListener('click', function(){
-    modalOverlay.classList.remove('.open-modal');
+    modalOverlay.classList.remove('open-modal');
 })
